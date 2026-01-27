a4626b7b42a3f1e84321779d700785789350276c
Yukiel <140352744+YukielCr@users.noreply.github.com>
2026-01-26 22:31:48 -0600

    Guardar Trabajos

--- Js/01.js
+++ Js/01.js
@@ -1,10 +1,25 @@
-// Manejo de la funcion fleca
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
 
-console.log("Hola Mubndo")
+for (let i = 0; i <= 100; i++) {
 
-for (let i=1;i<=10;i++){
-    if(i==5){
-        console.log("Estes es un mensaje "+i)
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log("fizzbuzz")
+    } else {
+        if (i % 5 == 0) {
+            console.log("buzz")
+        } else if (i % 3 == 0) {
+            console.log("fizz")
+        } else {
+            console.log(i)
+        }
     }
 }
-
+// nota multiplos de 3
+// \(\{0,3,6,9,12,15,18,21,24,27,30,\dots \}\)
---
+++ Js/02.js
@@ -0,0 +1,14 @@
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+let pa1="cola"
+let pa2="loca"
+
+function palsi(pa1,pa2){
+    if(pa1.)
+}
