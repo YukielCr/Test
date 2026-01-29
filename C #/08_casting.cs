2014b36f7937454be6080d6828ca0a4e3fc478a3
Yukiel <kevinyukielcoronaramos@gmail.com>
2026-01-28 18:55:41 -0600

    Este es un mensaje

---
+++ C #/08_casting.cs
@@ -0,0 +1,9 @@
+//casting implisito
+int valor = 200;
+int tatal = valor;
+Console.WriteLine(tatal);
+
+//casting explistito
+double precio = 500.23;
+int descuento = (int)precio;
+Console.WriteLine(descuento);   
